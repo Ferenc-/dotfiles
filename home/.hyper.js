3b5f6ec64f84b8577df9ac779e650a2defc639f6
Ferenc- <ferenc.gm@gmail.com>
2017-09-20 15:24:35 +0200

    Add custom padding to mainly unused hyper.js

--- home/.hyper.js
+++ home/.hyper.js
@@ -38,7 +38,7 @@ module.exports = {
     showWindowControls: '',
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: '2px 2px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
